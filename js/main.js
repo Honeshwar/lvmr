d1eca414649a7c6ed3af6fe77dc5c557d51869e1
Honeshwar Thakur <honeshwar.thakur@tailnode.com>
2025-05-25 12:25:26 +0530

    resolving navbar issue and some alignment styling

--- js/main.js
+++ js/main.js
@@ -16,24 +16,24 @@
   // Stellar
   $(window).stellar();
 
-  $("nav .dropdown").hover(
-    function () {
-      var $this = $(this);
-      $this.addClass("show");
-      $this.find("> a").attr("aria-expanded", true);
-      $this.find(".dropdown-menu").addClass("show");
-    },
-    function () {
-      var $this = $(this);
-      $this.removeClass("show");
-      $this.find("> a").attr("aria-expanded", false);
-      $this.find(".dropdown-menu").removeClass("show");
-    }
-  );
+  // $("nav .dropdown").hover(
+  //   function () {
+  //     var $this = $(this);
+  //     $this.addClass("show");
+  //     $this.find("> a").attr("aria-expanded", true);
+  //     $this.find(".dropdown-menu").addClass("show");
+  //   },
+  //   function () {
+  //     var $this = $(this);
+  //     $this.removeClass("show");
+  //     $this.find("> a").attr("aria-expanded", false);
+  //     $this.find(".dropdown-menu").removeClass("show");
+  //   }
+  // );
 
-  $("#dropdown04").on("show.bs.dropdown", function () {
-    console.log("show");
-  });
+  // $("#dropdown04").on("show.bs.dropdown", function () {
+  //   console.log("show");
+  // });
 
   // $("#dropdown04").on("click", function (e) {
   //   e.preventDefault();
